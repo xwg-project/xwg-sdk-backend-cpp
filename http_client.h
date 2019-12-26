70fdf6239f2bf87db46f34a6d251035833a6b3de
bigBox <guigu_pro.exe@163.com>
2019-12-26 14:27:39 +0800

    Add files via upload

---
+++ http_client.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <functional>
+#include <iostream>
+#include "common/mongoose.h"
+
+// 此处必须用function类，typedef再后面函数指针赋值无效
+//using ReqCallback = std::function<void (std::string)>;
+typedef void (*ReqCallback)(std::string);
+
+class HttpClient
+{
+public:
+	HttpClient() {}
+	~HttpClient() {}
+
+	static void SendReq(const std::string &url, ReqCallback req_callback);
+	static void PostReq(const std::string &url, ReqCallback req_callback, const std::string &data);
+	static void OnHttpEvent(mg_connection *connection, int event_type, void *event_data);
+	static int s_exit_flag;
+	static ReqCallback s_req_callback;
+};
